517e999c28b3751aa21bf66a86aaf4246c6734e4
eilseven <enginilseven@gmail.com>
2015-05-24 12:18:18 +0300

    Revert "Revert "Initial commit for Programming Assignment 2""
    This reverts commit 11c4fc324fc68d8a9399afb378dded823341943e.

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,15 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y     #set matrix x to a new matrix y
-                m <<- NULL  #resets the inverse to null
-        }
-        get <- function() x #returns matrix x
-        setinverse <- function(solve) m <<- solve #sets the inverse m, to solve
-        getinverse <- function() m #returns the inverse, m
-        list(set = set, get = get, #returns a special matrix
-             setinverse = setinverse,
-             getinverse = getinverse)
+
 }
 
-cacheSolve <- function(x, ...) { #calculates the inverse of the special matrix
-        m <- x$getinverse()
-        if(!is.null(m)) { #checks if inverse has already been calculated
-                message("getting cached data") #if already been calculated, gives a message
-                return(m) #and return the already-calculated inverse from the cache and skips the computation
-        }
-        data <- x$get() 
-        m <- solve(data, ...) #Otherwise, it calculates the inverse
-        x$setinverse(m) #and sets the value of inverse in the cache via the setinverse function
-        m
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
